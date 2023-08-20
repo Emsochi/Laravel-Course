b8260b13aac0e44efd977c4c20ac90b4c83f222f
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-08-20 18:03:03 +0200

    added subtitles

--- laravel-Instagram-Project/resources/views/dashboard.blade.php
+++ laravel-Instagram-Project/resources/views/dashboard.blade.php
@@ -1,25 +1,33 @@
-
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 
     <x-app-layout>
-    </head>
-    <body>
-    <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="row">
-            <div class="col-3 p-5">
-                <img src="https://yt3.googleusercontent.com/ytc/AOPolaTs1IEit9EUooQAJkWS4SkpUE7oMDXYrjIgnOk1Kw=s900-c-k-c0x00ffffff-no-rj" class="logoPhoto">
-            </div> 
-            <div class="col-9">
+</head>
 
+<body>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="row">
+                <div class="col-3 p-5">
+                    <img src="https://yt3.googleusercontent.com/ytc/AOPolaTs1IEit9EUooQAJkWS4SkpUE7oMDXYrjIgnOk1Kw=s900-c-k-c0x00ffffff-no-rj"
+                        class="logoPhoto">
+
+                    <div class="col-9 pt-5">
+                        <div>
+                            <h1>Instagram</h1>
+                        </div>
+                        <div class="instagramSubtitles">
+                            <div class="pr-4"><strong>153</strong> posts</div>
+                            <div class="pr-4"><strong>23k</strong> followers</div>
+                            <div class="pr-4"><strong>212</strong> following</div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-</div>
-    </div>
-</x-app-layout>
-
+        </x-app-layout>
