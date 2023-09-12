1c3dba281e59a502896b909e2b0433930e7a4a87
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-09-12 22:43:34 +0200

    added new description

--- laravel-Instagram-Project/resources/views/dashboard.blade.php
+++ laravel-Instagram-Project/resources/views/dashboard.blade.php
@@ -2,32 +2,52 @@
 <html>
 
 <head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-
-    <x-app-layout>
 </head>
+<x-app-layout>
 
-<body>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="row">
-                <div class="col-3 p-5">
-                    <img src="https://yt3.googleusercontent.com/ytc/AOPolaTs1IEit9EUooQAJkWS4SkpUE7oMDXYrjIgnOk1Kw=s900-c-k-c0x00ffffff-no-rj"
-                        class="logoPhoto">
+    <body>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="row">
+
+                    <div class="col-3 ">
+                        <div class="logoContainer">
+                           
+                            <img src="https://yt3.googleusercontent.com/ytc/AOPolaTs1IEit9EUooQAJkWS4SkpUE7oMDXYrjIgnOk1Kw=s900-c-k-c0x00ffffff-no-rj"
+                                class="logoPhoto ">
+                        </div>
+                    </div>
+
 
                     <div class="col-9 pt-5">
-                        <div>
-                            <h1>Instagram</h1>
-                        </div>
-                        <div class="instagramSubtitles">
-                            <div class="pr-4"><strong>153</strong> posts</div>
-                            <div class="pr-4"><strong>23k</strong> followers</div>
-                            <div class="pr-4"><strong>212</strong> following</div>
+                        <div class="titleContainer">
+                            
+                            <div class="instagramLogo">
+                                <h1>Instagram</h1>
+                            </div>
+                            <div class="instagramSubtitles">
+                                <div class="pr-4"><strong>153</strong> posts</div>
+                                <div class="pr-4"><strong>23k</strong> followers</div>
+                                <div class="pr-4"><strong>212</strong> following</div>
+                            </div>
+                            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+                            <div> We're a global community of millions of people learning to code together.
+                                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/ 
+                            </div>
+                            <div><a href="#"> www.freecodecamp.org</a></div>
 
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
-        </x-app-layout>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  
+    </body>
+</x-app-layout>
