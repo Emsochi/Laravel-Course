5827a655cf27742817813f6b872b61c51c410c2e
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-09-13 21:49:36 +0200

    added photo

--- laravel-Instagram-Project/resources/views/dashboard.blade.php
+++ laravel-Instagram-Project/resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 
+
 <head>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -14,9 +15,9 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="row">
 
-                    <div class="col-3 ">
+                    <div class="col-3 pt-5">
                         <div class="logoContainer">
-                           
+
                             <img src="https://yt3.googleusercontent.com/ytc/AOPolaTs1IEit9EUooQAJkWS4SkpUE7oMDXYrjIgnOk1Kw=s900-c-k-c0x00ffffff-no-rj"
                                 class="logoPhoto ">
                         </div>
@@ -25,7 +26,7 @@
 
                     <div class="col-9 pt-5">
                         <div class="titleContainer">
-                            
+
                             <div class="instagramLogo">
                                 <h1>Instagram</h1>
                             </div>
@@ -36,18 +37,28 @@
                             </div>
                             <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
                             <div> We're a global community of millions of people learning to code together.
-                                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/ 
+                                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/
                             </div>
                             <div><a href="#"> www.freecodecamp.org</a></div>
-
                         </div>
                     </div>
 
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-4">
+                <img src="https://images.pexels.com/photos/5700184/pexels-photo-5700184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Kodowanie">
+            </div>
+            <div class="col-4">
+                <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Zdjęcie laptopa">
+            </div>
+            <div class="col-4">
+                <img src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Zdjęcie z kartką">
+            </div>
+        </div>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-  
+
     </body>
 </x-app-layout>
