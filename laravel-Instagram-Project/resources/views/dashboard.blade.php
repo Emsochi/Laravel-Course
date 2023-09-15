6786f598c7a6debbc51b1eefe0f368c3fa48255d
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-09-15 23:00:04 +0200

    code formatted

--- laravel-Instagram-Project/resources/views/dashboard.blade.php
+++ laravel-Instagram-Project/resources/views/dashboard.blade.php
@@ -12,77 +12,86 @@
 
     <body>
         <div class="mx-40 px-20 pt-30">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="row">
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="row">
 
-                    <div class="col-3 pt-5">
-                        <div class="logoContainer">
+                        <div class="col-3 pt-5">
+                            <div class="logoContainer">
 
-                            <img src="https://yt3.googleusercontent.com/ytc/AOPolaTs1IEit9EUooQAJkWS4SkpUE7oMDXYrjIgnOk1Kw=s900-c-k-c0x00ffffff-no-rj"
-                                class="logoPhoto ">
+                                <img src="https://yt3.googleusercontent.com/ytc/AOPolaTs1IEit9EUooQAJkWS4SkpUE7oMDXYrjIgnOk1Kw=s900-c-k-c0x00ffffff-no-rj"
+                                    class="logoPhoto ">
+                            </div>
                         </div>
-                    </div>
 
 
-                    <div class="col-9 pt-5">
-                        <div class="titleContainer">
+                        <div class="col-9 pt-5">
+                            <div class="titleContainer">
 
-                            <div class="instagramLogo">
-                                <h1>Instagram</h1>
+                                <div class="instagramLogo">
+                                    <h1>Instagram</h1>
+                                </div>
+                                <div class="instagramSubtitles">
+                                    <div class="pr-4"><strong>153</strong> posts</div>
+                                    <div class="pr-4"><strong>23k</strong> followers</div>
+                                    <div class="pr-4"><strong>212</strong> following</div>
+                                </div>
+                                <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+                                <div> We're a global community of millions of people learning to code together.
+                                    LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/
+                                </div>
+                                <div><a href="#"> www.freecodecamp.org</a></div>
                             </div>
-                            <div class="instagramSubtitles">
-                                <div class="pr-4"><strong>153</strong> posts</div>
-                                <div class="pr-4"><strong>23k</strong> followers</div>
-                                <div class="pr-4"><strong>212</strong> following</div>
-                            </div>
-                            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-                            <div> We're a global community of millions of people learning to code together.
-                                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/
-                            </div>
-                            <div><a href="#"> www.freecodecamp.org</a></div>
                         </div>
-                    </div>
 
-                </div>
-            </div>
-        </div>
-        <div class="row pt-4 ">
-            <div class="col-4 ">
-                <img src="https://images.pexels.com/photos/4385547/pexels-photo-4385547.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Coding" class= "w-100">
-            </div>
-            <div class="col-4">
-                <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Laptop photo" class="w-100" >
-            </div>
-            <div class="col-4">
-                <img src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Photo with card" class="w-100">
-            </div>
-            <div class="row pt-4 ">
-                <div class="col-4 ">
-                    <img src="https://images.pexels.com/photos/1181244/pexels-photo-1181244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Phone and laptop" class= "w-100">
-                </div>
-                <div class="col-4">
-                    <img src="https://images.pexels.com/photos/1181325/pexels-photo-1181325.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Women in jacket " class="w-100" >
-                </div>
-                <div class="col-4">
-                    <img src="https://images.pexels.com/photos/1181676/pexels-photo-1181676.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Macbook support" class="w-100">
+                    </div>
                 </div>
             </div>
             <div class="row pt-4 ">
                 <div class="col-4 ">
-                    <img src="https://images.pexels.com/photos/270488/pexels-photo-270488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Clean Code" class= "w-100">
+                    <img src="https://images.pexels.com/photos/4385547/pexels-photo-4385547.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        alt="Coding" class="w-100">
                 </div>
                 <div class="col-4">
-                    <img src="https://images.pexels.com/photos/3861958/pexels-photo-3861958.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Women coding " class="w-100" >
+                    <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt="Laptop photo" class="w-100">
                 </div>
                 <div class="col-4">
-                    <img src="https://images.pexels.com/photos/1181373/pexels-photo-1181373.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Free use" class="w-100">
+                    <img src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        alt="Photo with card" class="w-100">
+                </div>
+                <div class="row pt-4 ">
+                    <div class="col-4 ">
+                        <img src="https://images.pexels.com/photos/1181244/pexels-photo-1181244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="Phone and laptop" class="w-100">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://images.pexels.com/photos/1181325/pexels-photo-1181325.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                            alt="Women in jacket " class="w-100">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://images.pexels.com/photos/1181676/pexels-photo-1181676.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                            alt="Macbook support" class="w-100">
+                    </div>
+                </div>
+                <div class="row pt-4 ">
+                    <div class="col-4 ">
+                        <img src="https://images.pexels.com/photos/270488/pexels-photo-270488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                            alt="Clean Code" class="w-100">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://images.pexels.com/photos/3861958/pexels-photo-3861958.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                            alt="Women coding " class="w-100">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://images.pexels.com/photos/1181373/pexels-photo-1181373.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                            alt="Free use" class="w-100">
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+            <div>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 
     </body>
