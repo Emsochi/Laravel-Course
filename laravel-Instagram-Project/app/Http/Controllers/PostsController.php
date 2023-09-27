bbfc342f8dfd49ce1adbdb4cb8c52fd729d16ac5
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-09-27 23:04:14 +0200

    new records created in the database

--- laravel-Instagram-Project/app/Http/Controllers/PostsController.php
+++ laravel-Instagram-Project/app/Http/Controllers/PostsController.php
@@ -23,11 +23,14 @@ class PostsController extends Controller
             'image' => ['required','image'], 
         ]);
 
-        dd(request('image')->store('/uploads','public'));
+        $imagePath = (request('image')->store('/uploads','public'));
 
-        auth()->user()->posts()->create($data);
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
 
         
-        dd(request()->all());
+        return redirect('/instagram/' . auth()->user()->id);
     }
 }
