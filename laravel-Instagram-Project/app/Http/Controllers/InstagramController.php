561a4e4eb3a78919771804f8c4a3455524d2d937
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-09-28 21:41:10 +0200

    added functions for displaying user profile

--- laravel-Instagram-Project/app/Http/Controllers/InstagramController.php
+++ laravel-Instagram-Project/app/Http/Controllers/InstagramController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\User;
 use Auth;
 
 class InstagramController extends Controller
 {
-    public function index(Request $request): View
+    public function index()
     {
         return view('dashboard', [
-            'user' => Auth::user(),
-
+            'user' => Auth::user()
         ]);
     }
+
+    public function show($user)
+    {
+        $user = User::findOrFail($user);
+        return view('dashboard', [
+            'user' => $user
+        ]);
+    }
+
 }
