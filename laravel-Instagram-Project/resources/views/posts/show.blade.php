8f926710e5f5fb573bf7af60eaf0c226867e1dfa
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-09-29 21:02:50 +0200

    created new blade to show single post

---
+++ laravel-Instagram-Project/resources/views/posts/show.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <x-app-layout>
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <img src="/storage/{{$post->image}}" class="w-100">
+                </div>
+                <div class="col-4">
+                    <div>
+                            <h1> {{$post->user->user_name}}</h1>
+
+                            <p>{{ $post->caption}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </x-app-layout>
+</body>
+
+</html>
