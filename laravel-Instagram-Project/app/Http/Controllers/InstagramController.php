7b4a898855b7a778cdd00bdc892eecf60feff31a
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-10-05 23:15:08 +0200

    handling image upload by the user

--- laravel-Instagram-Project/app/Http/Controllers/InstagramController.php
+++ laravel-Instagram-Project/app/Http/Controllers/InstagramController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use App\Models\User;
+use Intervention\Image\Facades\Image;
+
 use Auth;
 
 class InstagramController extends Controller
@@ -38,7 +40,19 @@ class InstagramController extends Controller
             'image'=>'',
         ]);
         
-        auth()->user()->profile->update($data);
+
+        if(request('image')) {
+            $imagePath = (request('image')->store('/instagram','public'));
+
+        $image= Image::make(public_path("storage/{$imagePath}"))->fit(1200,1200);
+        $image->save();
+        }
+
+        
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
 
         return redirect("/instagram/{$user->id}");
     }
