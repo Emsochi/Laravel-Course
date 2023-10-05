d6e3b561b3ad0897aec6b2dcafd3672e8236a886
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-10-05 23:14:06 +0200

    new record in the related "profiles" table for the user,

--- laravel-Instagram-Project/app/Models/User.php
+++ laravel-Instagram-Project/app/Models/User.php
@@ -43,6 +43,18 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($user)
+        {
+            $user->profile()->create([
+                    'title'=> $user->user_name,
+            ]);
+            });
+    }
+
     public function posts()
     {
         return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
