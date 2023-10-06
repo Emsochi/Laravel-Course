9a13369cdb68e125889ed80e02d3e3f7762a22d8
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-10-06 20:31:56 +0200

    created a post page

--- laravel-Instagram-Project/resources/views/posts/show.blade.php
+++ laravel-Instagram-Project/resources/views/posts/show.blade.php
@@ -15,18 +15,28 @@
         <div class="container">
             <div class="row">
                 <div class="col-8">
-                    <img src="/storage/{{$post->image}}" class="w-100">
+                    <img src="/storage/{{ $post->image }}" class="w-100">
                 </div>
                 <div class="col-4">
                     <div>
-                            <h1> {{$post->user->user_name}}</h1>
-
-                            <p>{{ $post->caption}}</p>
+                        <div class="d-flex align-items-center">
+                            <div class="pr-3">
+                                <img src="/storage/{{ $post->user->profile->image }}" class="rounded-circle w-100"
+                                    style="max-width: 40px">
+                            </div>
+                            <div>
+                                <div class="font-weight-bold">{{ $post->user->user_name }}</div>
+                            </div>
+                        </div>
+                        <hr>
+                        <p><span class="font-weight-bold">{{ $post->user->user_name }}</span>{{$post->caption}}</p>
                     </div>
                 </div>
             </div>
         </div>
+        </div>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </x-app-layout>
 </body>
+
 </html>
