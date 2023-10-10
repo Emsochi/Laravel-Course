d15da319f863bc7c51e8f6d59dd2f228d8f5054e
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-10-10 21:56:49 +0200

    contruct function added

--- laravel-Instagram-Project/app/Http/Controllers/FollowsController.php
+++ laravel-Instagram-Project/app/Http/Controllers/FollowsController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\User;
+
+
+use Auth;
 
 class FollowsController extends Controller
 {
-    public function follow(Request $request, $user)
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function store(Request $request, \App\Models\User $user)
     {
         return auth()->user()->following()->toggle($user->profile);
     }
